c2c72f532be85af668ac7a6cb587114624bb8f08
hellochoki <audition36@skku.edu>
2022-12-03 20:52:24 +0900

    [INIT] Spring Init

---
+++ src/main/java/com/yapp/ios2/fitfty/FitftyApplication.java
@@ -0,0 +1,13 @@
+package com.yapp.ios2.fitfty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitftyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitftyApplication.class, args);
+	}
+
+}
