74443f6f1b18b63c855a517dbc7b18845a3bd88e
Somi Kim <rlathal009@gmail.com>
2022-12-29 21:55:35 +0900

    ✨ [feat]    : BaseException 파일 추가

---
+++ src/main/java/com/yapp/ios2/fitfty/global/exception/BaseException.java
@@ -0,0 +1,27 @@
+package com.yapp.ios2.fitfty.global.exception;
+
+import com.yapp.ios2.fitfty.global.response.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class BaseException extends RuntimeException {
+    private ErrorCode errorCode;
+
+    public BaseException() {
+    }
+
+    public BaseException(ErrorCode errorCode) {
+        super(errorCode.getErrorMsg());
+        this.errorCode = errorCode;
+    }
+
+    public BaseException(String message, ErrorCode errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public BaseException(String message, ErrorCode errorCode, Throwable cause) {
+        super(message, cause);
+        this.errorCode = errorCode;
+    }
+}
