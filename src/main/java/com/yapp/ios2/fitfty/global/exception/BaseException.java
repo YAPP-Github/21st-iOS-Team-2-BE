8cf9a62d226401dd35b7ccea278053cf58ea908d
Somi Kim <rlathal009@gmail.com>
2022-12-30 00:05:08 +0900

    ♻️ [refactor] : ErrorCode 기본 생성자 제거

--- src/main/java/com/yapp/ios2/fitfty/global/exception/BaseException.java
+++ src/main/java/com/yapp/ios2/fitfty/global/exception/BaseException.java
@@ -7,9 +7,6 @@ import lombok.Getter;
 public class BaseException extends RuntimeException {
     private ErrorCode errorCode;
 
-    public BaseException() {
-    }
-
     public BaseException(ErrorCode errorCode) {
         super(errorCode.getErrorMsg());
         this.errorCode = errorCode;
