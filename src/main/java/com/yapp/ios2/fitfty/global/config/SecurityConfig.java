11379ae5ea0d7b992153012bd41b67291eacda00
hellochoki <audition36@skku.edu>
2023-01-02 23:15:27 +0900

    ✨ [feat] : jwt 관련 설정 추가
    - jwt 관련 의존성 추가 (build.gradle)
    - jwt 관련 설정 추가 (application-dev.yaml)
    - Jwt 관련 객체 추가
    - Jwt 관련 Config 수정
    - 실행 확인만 하면서 일단 구현, 프로젝트 구조에 맞게 Refactoring 진행하여 PR 진행 예정

--- src/main/java/com/yapp/ios2/fitfty/global/config/SecurityConfig.java
+++ src/main/java/com/yapp/ios2/fitfty/global/config/SecurityConfig.java
@@ -1,24 +1,71 @@
 package com.yapp.ios2.fitfty.global.config;
 
+import com.yapp.ios2.fitfty.domain.auth.JwtAccessDeniedHandler;
+import com.yapp.ios2.fitfty.domain.auth.JwtAuthenticationEntryPoint;
+import com.yapp.ios2.fitfty.domain.auth.JwtSecurityConfig;
+import com.yapp.ios2.fitfty.domain.auth.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+@RequiredArgsConstructor
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableMethodSecurity
+@Configuration
+public class SecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
-        // h2 iframe 관련 허용
-        http.headers()
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                // token을 사용하는 방식이기 때문에 csrf를 disable합니다.
+                .csrf()
+                .disable()
+
+                // Custom Class Injection
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+
+                // enable h2-console
+                .and()
+                .headers()
                 .frameOptions()
-                .sameOrigin();
+                .sameOrigin()
 
-        // 일단 csrf 전체 풀어 둠
-        http.csrf()
-                .disable();
+                // 세션을 사용하지 않기 때문에 STATELESS로 설정
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeHttpRequests()
+                .antMatchers("/api/v1/user").permitAll()
+                .antMatchers("/api/v1/user/**").permitAll()
+                .requestMatchers(PathRequest.toH2Console())
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+
+                .and()
+                .apply(new JwtSecurityConfig(tokenProvider));
+
+        return httpSecurity.build();
     }
 }
