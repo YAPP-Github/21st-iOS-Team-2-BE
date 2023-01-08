280ac7d3d4f0b117202ec357e0aa8dca16399812
hellochoki <audition36@skku.edu>
2023-01-08 12:44:39 +0900

    ✨ [feat] : Kakao Access token Request 구현
    - 요청 받은 Auth Code로 AccessToken 받아오는 것까지 확인
    - Todo : Kakao Access Token 으로 사용자 값 불러오고, 내부 User Entity 와 연계하여 로그인 서비스 구현

--- src/main/java/com/yapp/ios2/fitfty/interfaces/auth/AuthController.java
+++ src/main/java/com/yapp/ios2/fitfty/interfaces/auth/AuthController.java
@@ -5,22 +5,28 @@ import com.yapp.ios2.fitfty.domain.auth.UserDto;
 import com.yapp.ios2.fitfty.domain.auth.UserService;
 import com.yapp.ios2.fitfty.global.exception.MemberNotFoundException;
 import com.yapp.ios2.fitfty.global.response.CommonResponse;
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -80,6 +86,26 @@ public class AuthController {
 
     @GetMapping("/auth/kakao/callback")
     public CommonResponse HandleKakakoOAuth(String code) {
-        return CommonResponse.success(code);
+
+        RestTemplate rt = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", "31e815075078c89d7505decdeed8af98");
+        params.add("redirect_uri", "http://localhost:8080/api/v1/auth/kakao/callback");
+        params.add("code", code);
+
+        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(params, headers);
+
+        ResponseEntity<String> responseEntity = rt.exchange(
+                "https://kauth.kakao.com/oauth/token",
+                HttpMethod.POST,
+                kakaoTokenRequest,
+                String.class
+        );
+
+        return CommonResponse.success(responseEntity);
     }
 }
