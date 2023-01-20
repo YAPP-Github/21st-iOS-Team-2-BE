b45db8054819f38386531c18e3d9a786624e22d2
Somi Kim <rlathal009@gmail.com>
2023-01-20 22:24:15 +0900

    ♻️ [refactor]: 불필요한 코드 제거

--- src/main/java/com/yapp/ios2/fitfty/infrastructure/picture/BoardStoreImpl.java
+++ src/main/java/com/yapp/ios2/fitfty/infrastructure/picture/BoardStoreImpl.java
@@ -24,11 +24,4 @@ public class BoardStoreImpl implements BoardStore {
         return pictureRepository.save(picture);
     }
 
-//    private void validCheck(Item item) {
-//        if (StringUtils.isEmpty(item.getItemToken())) throw new InvalidParamException("Item.itemToken");
-//        if (StringUtils.isEmpty(item.getItemName())) throw new InvalidParamException("Item.itemName");
-//        if (item.getPartnerId() == null) throw new InvalidParamException("Item.partnerId");
-//        if (item.getItemPrice() == null) throw new InvalidParamException("Item.itemPrice");
-//        if (item.getStatus() == null) throw new InvalidParamException("Item.status");
-//    }
 }
