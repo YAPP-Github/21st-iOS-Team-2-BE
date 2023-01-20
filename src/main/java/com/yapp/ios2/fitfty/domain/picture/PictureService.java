24bc4d2634263aa3d72970cbc5557442800faecd
Somi Kim <rlathal009@gmail.com>
2023-01-20 16:08:18 +0900

    ✨ [feat]    : 게시글 조회, 삭제 api 생성

--- src/main/java/com/yapp/ios2/fitfty/domain/picture/PictureService.java
+++ src/main/java/com/yapp/ios2/fitfty/domain/picture/PictureService.java
@@ -2,7 +2,10 @@ package com.yapp.ios2.fitfty.domain.picture;
 
 public interface PictureService {
     Board registerBoard(PictureCommand.RegisterBoardRequest request);
-//    void changeTag(PictureCommand.ChangeTagRequest request, String userToken);
+
+    void deleteBoard(String boardToken);
+
+    //    void changeTag(PictureCommand.ChangeTagRequest request, String userToken);
 //    void changeBoardInfo(PictureCommand.ChangeBoardInfo request, String userToken);
     BoardInfo.Main retrieveBoardInfo(String boardToken);
 }
--- src/main/java/com/yapp/ios2/fitfty/interfaces/picture/BoardController.java
+++ src/main/java/com/yapp/ios2/fitfty/interfaces/picture/BoardController.java
@@ -1,6 +1,6 @@
 package com.yapp.ios2.fitfty.interfaces.picture;
 
-import com.yapp.ios2.fitfty.application.picture.PictureFacade;
+import com.yapp.ios2.fitfty.domain.picture.PictureService;
 import com.yapp.ios2.fitfty.global.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,13 +22,13 @@ import static com.yapp.ios2.fitfty.global.util.Constants.API_PREFIX;
 @RequiredArgsConstructor
 @RequestMapping(API_PREFIX + "/boards")
 public class BoardController {
-    private final PictureFacade pictureFacade;
+    private final PictureService pictureService;
     private final PictureDtoMapper pictureDtoMapper;
 
     @PostMapping("/new")
     public CommonResponse registerBoard(@RequestBody @Valid BoardDto.RegisterBoardRequest request) {
         var pictureCommand = pictureDtoMapper.of(request);
-        var board = pictureFacade.registerBoard(pictureCommand);
+        var board = pictureService.registerBoard(pictureCommand);
         var response = pictureDtoMapper.of(board);
         return CommonResponse.success(response);
     }
@@ -40,17 +40,16 @@ public class BoardController {
 //        return CommonResponse.success("OK");
 //    }
 //
-//    @DeleteMapping("/{boardToken}")
-//    public CommonResponse deleteBoard(@PathVariable("boardToken") String boardToken) {
-//        var userToken =
-//        pictureFacade.deleteBoard(boardToken);
-//        return CommonResponse.success("OK");
-//    }
+    @DeleteMapping("/{boardToken}")
+    public CommonResponse deleteBoard(@PathVariable("boardToken") String boardToken) {
+        pictureService.deleteBoard(boardToken);
+        return CommonResponse.success("OK");
+    }
 
-//    @GetMapping("/{boardToken}")
-//    public CommonResponse retrieve(@PathVariable("boardToken") String boardToken) {
-//        var boardInfo = pictureFacade.retrieveBoardInfo(boardToken);
-//        var response = pictureDtoMapper.of(boardInfo);
-//        return CommonResponse.success(response);
-//    }
+    @GetMapping("/{boardToken}")
+    public CommonResponse retrieve(@PathVariable("boardToken") String boardToken) {
+        var boardInfo = pictureService.retrieveBoardInfo(boardToken);
+        var response = pictureDtoMapper.of(boardInfo);
+        return CommonResponse.success(response);
+    }
 }
