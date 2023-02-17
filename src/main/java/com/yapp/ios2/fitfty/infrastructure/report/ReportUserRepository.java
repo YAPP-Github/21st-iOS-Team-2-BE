e4a1b2e60cf0090b1655bbad2d9f6b8a370e223c
Somi Kim <rlathal009@gmail.com>
2023-02-17 23:46:11 +0900

    ✨ [feat]    : reportUser, reportBoard 레포에서 신고목록 찾아오기

--- src/main/java/com/yapp/ios2/fitfty/infrastructure/report/ReportUserRepository.java
+++ src/main/java/com/yapp/ios2/fitfty/infrastructure/report/ReportUserRepository.java
@@ -1,12 +1,19 @@
 package com.yapp.ios2.fitfty.infrastructure.report;
 
+import com.yapp.ios2.fitfty.domain.report.ReportBoard;
 import com.yapp.ios2.fitfty.domain.report.ReportUser;
+
 import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReportUserRepository extends JpaRepository<ReportUser, Long> {
 
     Optional<ReportUser> findByReportToken(String reportToken);
 
-    Optional<ReportUser> findFirstByReportedUserTokenOrderByReportedCountDesc(String reportedUserToken);
+    Optional<ReportBoard> findFirstByReportUserTokenAndReportedUserToken(String currentUserToken,
+                                                                         String reportedUserToken);
+
+    Optional<ReportUser> findFirstByReportedUserTokenOrderByReportedCountDesc(
+            String reportedUserToken);
 }
